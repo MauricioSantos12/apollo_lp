0fdc6e5a749681cd7528739e159c2ccd59d723d4
mauricios4l <efrain.santos@s4learning.com>
2026-04-14 12:24:20 -0500

    refactor: reorder section

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ export default function App() {
         <QuoteSection />
         <IdentitySection />
         <ServicesTabs />
-        <CompaniesSection />
         <Contact />
+        <CompaniesSection />
       </Box>
       <Footer />
     </Box>
